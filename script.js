9514d1b805e4d061142cb6ae148017fcd0aac457
Giovanni Bianchini <81379937+Bianco54@users.noreply.github.com>
2022-05-13 16:06:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     /*
     you only have to call function with the following paramaters:
     1) General container
-    2) Which element inside container will be hidden
+    2) Which type of element inside container will be hidden
     3) How many items show at first
     4) How many items add to list every time "show more" is triggered
     */
